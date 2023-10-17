4f74fa4c9f4ec8a5f2d811a507e5f4d40ee973dd
TaegonChung <sunny2ys@dgist.ac.kr>
2023-10-17 15:18:55 +0900

    Added more examples

--- examples/example04.py
+++ examples/example04.py
@@ -5,11 +5,8 @@ fps = 32
 kymogram = np.load('kymogram_delta-turn.npy')
 
 env = Worm(
-    simTime=(kymogram.shape[0]-1)/fps,
-    dt_snapshot=1/fps,
     scale_friction=0.01,
     polarity_clockwise=True,
 )
-
 env.run(kymogram, 1/fps)
 env.play_animation()
